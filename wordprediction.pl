111dc02508792ab2343a28494b33fd2e8c581772
Nick Burris <nickburris13@gmail.com>
2018-03-24 12:44:44 -0300

    Addition of stree, but bash for windows is having trouble with it, moving to debian machine

--- wordprediction.pl
+++ wordprediction.pl
@@ -2,8 +2,22 @@
 use strict;
 use warnings;
 
+use Tree::Suffix;
+
 sub gen_data_structures {
     my $dict_filename = shift;
+    my @words = read_file($dict_filename, chomp => 1);
+    $tree = Tree::Suffix->new(@words);
+}
+
+sub get_suffixes {
+    my $query = shift;
+    @matches = $tree->search($query);
+    return @matches;
 }
 
 gen_data_structures("words");
+
+@suffixes = get_suffixes("comp");
+
+print "$_\n" for @suffixes;
