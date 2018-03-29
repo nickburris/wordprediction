91b79e5bf4bc7ca2f1aff5423835ddd4136d55cb
Nick Burris <nickburris13@gmail.com>
2018-03-29 09:06:06 -0300

    wordprediction.py in progress

--- wordprediction.py
+++ wordprediction.py
@@ -1,19 +1,29 @@
+from ngram import NGram
 import pygtrie
 import re
 
 t = pygtrie.CharTrie()
+n = NGram()
 
-def gen_data_structures(filename):
-    reg = re.compile('[^a-zA-Z]')
+# Assume one word per line, like linux words
+def gen_trie(filename):
     with open(filename) as f:
-        for sentence in f:
-            for word in sentence.split(" "):
-                t[reg.sub('', word)] = True
+        for word in f:
+			t[word] = True
+				
+# In progress
+def gen_ngrams(filename):
+	reg = re.compile('[^a-zA-Z]')
+	with open(filename) as f:
+		for sentence in f:
+			for w in sentence.split(" "):
+				word = reg.sub('', w)
 
 def get_suffixes(query):
     return t.iterkeys(query)
 
-gen_data_structures("tomsawyer.txt")
+gen_trie("words")
+gen_ngrams("tomsawyer.txt")
 
-for word in get_suffixes('r'):
+for word in get_suffixes('comp'):
     print(word)
