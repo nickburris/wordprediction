d6288ca151ae1758ae5453c26164d4acc815b1fa
Nick Burris <nickburris13@gmail.com>
2018-04-05 19:18:29 -0300

    Add eval modes for other books for research

--- wordprediction.py
+++ wordprediction.py
@@ -10,12 +10,17 @@ import argparse
 # Start interactive mode where the user can enter words and get completions
 # and predictions
 M_INT = "interactive"
+# Train on brown and evaluate on brown
 M_EBROWN = "evalbrown"
+# Train on dracula and evaluate on frankenstein
+M_EDRACFRANK = "evaldracfrank"
+# Train on heart of darkness and evaluate on alice in wonderland
+M_EDARKWOND = "evaldarkwond"
 
 parser = argparse.ArgumentParser(
 	description="Evaluation of ngram based word completion and prediction.")
 parser.add_argument("-m", "--mode", help="the mode to run in",
-	choices=[M_INT, M_EBROWN], default=M_INT)
+	choices=[M_INT, M_EBROWN, M_EDRACFRANK, M_EDARKWOND], default=M_INT)
 args = parser.parse_args()
 
 nltk.download("brown")
@@ -31,7 +36,18 @@ def gen_trie(filename):
 			w = word.rstrip().lower()
 			if(w != ''):
 				t[w] = True
-				
+
+def get_raw_words(filename):
+	words = []
+	reg = re.compile('[^a-zA-Z]')
+	with open(filename, encoding="utf8") as f:
+		for sentence in f:
+			for w in sentence.split(" "):
+				w = reg.sub('', w).lower()
+				if(w != ''):
+					words.append(w)
+	return words
+
 # Generate bigrams and trigrams for the input texts
 # If dirname is empty, load words from filename (used for training on single files)
 ngram_words = []
@@ -93,7 +109,7 @@ def is_whole_word(w):
 # If last and possibly second_last are passed, results are based on ngrams
 def get_completions(partial, last = '', second_last = ''):
 	COUNT = 10
-	completions = None
+	completions = []
 	if last == '' and second_last == '':
 		# Just return the most common completions
 		completions = sorted([(w, c[w]) for w in get_suffixes(partial) if w in c],
@@ -131,23 +147,55 @@ def get_completions(partial, last = '', second_last = ''):
 def time_millis():
 	return int(round(time.time()*1000))
 	
-def get_predictions(last = '', second_last = ''):
-	N_PREDS = 10
+def get_predictions(last = '', second_last = '', N_PREDS=3):
+	preds = []
 	# Return predictions based on trigrams
 	if last != '' and second_last != '':
 		if second_last in ngrams and last in ngrams[second_last]:
-			return sorted([k for k,_ in ngrams[second_last][last].items()
+			preds = sorted([k for k,_ in ngrams[second_last][last].items()
 							if k != "_count"
 							and "_count" in ngrams[second_last][last][k]],
 							key=lambda k: ngrams[second_last][last][k]["_count"],
 							reverse=True)[:N_PREDS]
-	elif last != '':
+	
+	if last != '' or preds == []:
 		if last in ngrams:
-			return sorted([k for k,_ in ngrams[last].items()
+			preds = sorted([k for k,_ in ngrams[last].items()
 							if "_count" in ngrams[last][k]],
 							key=lambda k: ngrams[last][k]["_count"],
 							reverse=True)[:N_PREDS]
+	
+	return preds
 
+# Generate structures from struct_file and evaluate on raw_words
+def evaluate(struct_file, raw_words):
+	print("Generating structures...")
+	start_millis = time_millis()
+	gen_trie("words")
+	gen_ngrams(filename=struct_file)
+	gen_ngrams()
+	print("Generating structures took " + str((time_millis() - start_millis)) + "ms")
+
+	top_result = 0
+	top_3 = 0
+	total = 0
+	
+	for i in range(len(raw_words)-2):
+		predictions = get_predictions(last=raw_words[i+1], second_last=raw_words[i])
+		actual = raw_words[i+2]
+		if len(predictions) > 0:
+			if actual in predictions:
+				top_3 += 1
+			if actual == predictions[0]:
+				top_result += 1
+		total += 1
+		
+		# Print status every 10%
+		if i%(int(len(raw_words)/10)) == 0:
+			print("total: %d, top 3: %d, top result: %d" % (total, top_3, top_result))
+	print("total: %d, top 3: %d, top result: %d" % (total, top_3, top_result))
+	print("Correct top 3: %d%%, correct top result: %d%%" % (top_3*100/total, top_result*100/total))
+	
 #------------------------------
 
 # Interactive mode
@@ -199,32 +247,13 @@ if(args.mode == M_INT):
 			print(', '.join(completions))
 			print()
 
-# Evaluate algorithm on brown data
+# Train and evaluate algorithm on brown data
 elif args.mode == M_EBROWN:
-	print("Generating structures...")
-	start_millis = time_millis()
-	gen_trie("words")
-	gen_ngrams(filename="brown.txt")
-	gen_ngrams()
+	# ngram_words are the raw words from brown.txt
+	evaluate("brown.txt", get_raw_words("brown.txt"))
+	
+elif args.mode == M_EDRACFRANK:
+	evaluate("train_english/dracula.txt", get_raw_words("train_english/frankenstein.txt"))
 
-	print("Generating structures took " + str((time_millis() - start_millis)) + "ms")
-	
-	top_result = 0
-	top_ten = 0
-	total = 0
-	
-	# ngram_words are the words from brown
-	brown_words = ngram_words
-	for i in range(len(brown_words)-2):
-		predictions = get_predictions(last=brown_words[i+1], second_last=brown_words[i])
-		actual = brown_words[i+2]
-		if actual in predictions:
-			top_ten += 1
-		if actual == predictions[0]:
-			top_result += 1
-		total += 1
-		
-		# Print status every 10%
-		if i%(int(len(brown_words)/10)) == 0:
-			print("total: %d, top 10: %d, top result: %d" % (total, top_ten, top_result))
-	print("total: %d, top 10: %d, top result: %d" % (total, top_ten, top_result))
+elif args.mode == M_EDARKWOND:
+	evaluate("train_english/heartofdarkness.txt", get_raw_words("train_english/aliceinwonderland.txt"))
